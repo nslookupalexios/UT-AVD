e51b24788abfa80079c2703716a51efd5faa403e
nslookupalexios <aguarini@unisa.it>
2025-07-28 13:12:56 +0300

    practice 1 done

---
+++ src/autoware_mini_practice_solutions/src/publisher.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import String
+
+rospy.init_node('publisher')
+rate = rospy.Rate(2)
+pub = rospy.Publisher('/message', String, queue_size=10)
+
+while not rospy.is_shutdown():
+    pub.publish("Hello world!")
+    rate.sleep()
---
+++ src/autoware_mini_practice_solutions/src/subscriber.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import String
+
+def callback(msg):
+    print(msg.data)
+
+rospy.init_node('subscriber')
+rospy.Subscriber("/message", String, callback)
+rospy.spin()
