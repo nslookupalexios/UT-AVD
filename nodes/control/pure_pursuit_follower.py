0448b532e446274e9f14cde65f18812653a3f6e8
nslookupalexios <aguarini@unisa.it>
2025-07-30 12:54:07 +0300

    practice 3 - part 3 completed

--- nodes/control/pure_pursuit_follower.py
+++ nodes/control/pure_pursuit_follower.py
@@ -3,6 +3,7 @@ import rospy
 
 from autoware_mini.msg import Path
 from geometry_msgs.msg import PoseStamped
+from autoware_mini.msg import VehicleCmd
 
 class PurePursuitFollower:
     def __init__(self):
@@ -10,16 +11,23 @@ class PurePursuitFollower:
         # Parameters
 
         # Publishers
+        self.vehicle_cmd_pub = rospy.Publisher('/control/vehicle_cmd', VehicleCmd, queue_size=10)
 
         # Subscribers
         rospy.Subscriber('path', Path, self.path_callback, queue_size=1)
         rospy.Subscriber('/localization/current_pose', PoseStamped, self.current_pose_callback, queue_size=1)
 
     def path_callback(self, msg):
-        print(msg.header.seq)
+        pass
 
     def current_pose_callback(self, msg):
-        print(msg.header.seq)
+        vehicle_cmd = VehicleCmd()
+        vehicle_cmd.ctrl_cmd.steering_angle = 0.2
+        vehicle_cmd.ctrl_cmd.linear_velocity = 10.0
+
+        vehicle_cmd.header.frame_id = "base_link"
+        vehicle_cmd.header.stamp = msg.header.stamp
+        self.vehicle_cmd_pub.publish(vehicle_cmd)
 
     def run(self):
         rospy.spin()
